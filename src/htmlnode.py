a0a3fa41033e26a6f9921551a5045babd1a40f48
shkmanu <manu.candia@gmail.com>
2025-04-01 16:08:53 -0400

    F: Create LeafNode and Test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,3 +22,18 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props):
+        super().__init__(tag, value, None, props)
+        
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value required")
+        elif self.tag is None:
+            return f"{self.value}"
+        elif self.props is None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,19 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        html1 = HTMLNode(None, None, None, {"a": "123", "b": "456"})
+        html1 = HTMLNode("a", "This is me", None, {"href": "google.com", "target": "_blank"})
         compare1 = HTMLNode.props_to_html(html1)
-        compare2 = f'a="123" b="456"'
+        compare2 = 'href="google.com" target="_blank"'
+        self.assertEqual(compare1, compare2)
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        leaf1 = LeafNode("a", "This is me", {"abc": "def"})
+        compare1 = LeafNode.to_html(leaf1)
+        compare2 = '<a abc="def">This is me</a>'
         self.assertEqual(compare1, compare2)
 
 if __name__ == "__main__":
